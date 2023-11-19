d85d98c88e96dc3f0240f211ebaeea49db19603b
Olli <102247940+olliklee@users.noreply.github.com>
2023-11-20 00:29:38 +0100

    Update exercise.cc

--- 02_Basics/_Exercise/Exercise/exercise.cc
+++ 02_Basics/_Exercise/Exercise/exercise.cc
@@ -2,6 +2,21 @@
 
 #include "exercise.h"
 
+void print_even(int num)
+{
+    std::cout << std::boolalpha << num % 2 == 0 << std::endl; 
+}
+
+
 void mod_cross_sum(int I, int J)
 {
+    for (std::int16_t i= 0; i<I; i++)
+    {
+        for (std::int16_t j = 0; j<J; j++)
+        {
+            std::int16_t result = i+j;
+            std::cout << i << " + " << j << " is even? ";
+            print_even(result);
+        }
+    }
 }
